09b8aac7635d5ec0d41343e2cbd219839ce177a4
William Jones <wtravisjones@gmail.com>
2015-11-12 17:44:00 -0600

    Fix shape point. "you had ONE job"

--- shapes.py
+++ shapes.py
@@ -36,7 +36,7 @@ shapes.append(shape)
 # #  @
 # #
 shape = {'transforms': []}
-shape['points'] = [[-1, 1, 0], [0, 1, 0], [0, 0, 0], [0, 0, -1]]
+shape['points'] = [[-1, 1, 0], [0, 1, 0], [0, 0, 0], [0, 0, 1]]
 shapes.append(shape)
 
 # #  **
